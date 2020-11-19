ff672ff32b1ed1f78e6c050562a91862ccff54b7
David Jungst <david.jungst@divvypay.com>
2020-11-19 10:06:07 -0700

    agents name camel cased

--- modules/components/agents/Agents.js
+++ modules/components/agents/Agents.js
@@ -19,6 +19,8 @@ class Agents extends React.Component {
       const formattedAgents = agents.map( agent => {
         return {
           ...agent,
+          firstName: agent.firstname,
+          lastName: agent.lastname,
           url: agent.url[0] === '/' ? agent.url.substring(1) : agent.url
         }
       })
