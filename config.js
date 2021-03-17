cfa042f4e18f6c486e598ab557a0a2329b1d59ba
David Jungst <david.jungst@divvypay.com>
2021-03-17 11:24:18 -0600

    pg config

--- config.js
+++ config.js
@@ -4,12 +4,15 @@ if (!isProduction) {
   require('dotenv').config()
 }
 
-const { Pool } = require('pg')
+const { Client, Pool } = require('pg')
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+const pool = new Client({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    refjectUnauthorized: false
+  }
 })
 
 module.exports = { pool }
