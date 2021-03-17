7e1439dc926ca0d55ec9160df35a08759e84525e
David Jungst <david.jungst@divvypay.com>
2021-03-17 11:37:36 -0600

    ldsjf

--- config.js
+++ config.js
@@ -9,10 +9,7 @@ const { Pool } = require('pg')
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    refjectUnauthorized: false
-  }
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
 })
 
 module.exports = { pool }
