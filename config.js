6c623eabb4ee86d3e2baeeacc3e98e2ffd300408
David Jungst <david.jungst@divvypay.com>
2021-03-17 11:32:32 -0600

    asdf

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ if (!isProduction) {
   require('dotenv').config()
 }
 
-const { Client, Pool } = require('pg')
+const { Pool } = require('pg')
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Client({
+const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: {
     refjectUnauthorized: false
