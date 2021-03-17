70eed90434b1aa6d75b70b7f680088c2b16b5f94
David Jungst <david.jungst@divvypay.com>
2021-03-17 10:50:07 -0600

    require ssl connection

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (!isProduction) {
 
 const { Pool } = require('pg')
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?ssl=true`
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString
