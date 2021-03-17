9cd820dc0e1c0d8203fb4b96de26db5a4197c06e
David Jungst <david.jungst@divvypay.com>
2021-03-17 11:01:43 -0600

    foo

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ if (!isProduction) {
   require('dotenv').config()
 }
 
-const { Pool } = require('pg')
+const pg = require('pg')
+const { Pool } = pg
+pg.defaults.ssl = true
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?ssl=true`
 
