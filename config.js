d639a0e257372b63864e5f478edfad1d2b94c140
David Jungst <david.jungst@divvypay.com>
2021-03-17 11:20:05 -0600

    remove ssl

--- config.js
+++ config.js
@@ -4,11 +4,9 @@ if (!isProduction) {
   require('dotenv').config()
 }
 
-const pg = require('pg')
-const { Pool } = pg
-pg.defaults.ssl = true
+const { Pool } = require('pg')
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?ssl=true`
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString
